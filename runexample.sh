375112035955aa2b6761d537b87e7640c1bd71d1
Emre Sefer Research <70752445+seferlab@users.noreply.github.com>
2021-10-12 08:32:55 +0300

    minor fix

--- runexample.sh
+++ runexample.sh
@@ -5,8 +5,8 @@ sbt 'runMain LearnGraph --cfg examples/PrefAttach/PrefAttachExp.params'
 sbt 'runMain GrowCode.GrowGraph -g examples/PrefAttach/PrefAttach.edg -i 498 -o examples/PrefAttach/PrefAttach.grw examples/PrefAttach/PrefAttach.grw'
 
 # Evolve an ensemble of programs for the PPI network
-#sbt 'run-main LearnGraph --cfg examples/YeastPPI/YeastPPIExp.params'
+#sbt 'runMain LearnGraph --cfg examples/YeastPPI/YeastPPIExp.params'
 
 # Analyze the ensemble and report the 'best' program
-#sbt 'run-main GrowCode.GrowGraph -g examples/YeastPPI/YeastPPI.edg -i 2645 -o examples/YeastPPI/YeastPPIBest.grw examples/YeastPPI/YeastPPI.grw'
+#sbt 'runMain GrowCode.GrowGraph -g examples/YeastPPI/YeastPPI.edg -i 2645 -o examples/YeastPPI/YeastPPIBest.grw examples/YeastPPI/YeastPPI.grw'
 
